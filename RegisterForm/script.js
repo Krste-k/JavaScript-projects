5d99aef0c4f906902f3259952332d99081c58afa
Krste-k <krstevskikrste89@yahoo.com>
2023-04-17 18:58:57 +0200

    LoginForm

---
+++ RegisterForm/script.js
@@ -0,0 +1,11 @@
+let wrapper = document.querySelector('.wrapper')
+let loginLink = document.querySelector('.login-link')
+let registerLink = document.querySelector('.register-link')
+
+registerLink.addEventListener('click', () =>{
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', () =>{
+    wrapper.classList.remove('active');
+});
